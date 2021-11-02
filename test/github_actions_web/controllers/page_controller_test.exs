70995cc5aa08ff6d15f8a4ab153fb25af077fee7
Brett Bim <brettbim@Bretts-MacBook-Pro.local>
2021-11-02 14:07:39 -0500

    breaking a test to see what happens

--- test/github_actions_web/controllers/page_controller_test.exs
+++ test/github_actions_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule GithubActionsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix1!"
   end
 end
