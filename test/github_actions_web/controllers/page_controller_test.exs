080a0934d78039e2c0a387b112d7ef80dea66ca6
Brett Bim <brettbim@protonmail.com>
2021-11-02 14:30:42 -0500

    maybe with some honeycomb

--- test/github_actions_web/controllers/page_controller_test.exs
+++ test/github_actions_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule GithubActionsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix1!"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 end
